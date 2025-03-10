fcd79cafde631ab007e8daa642f325ca0624426c
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-10 15:33:18 +0100

    Ajout GET Epreuves

--- app/src/main/java/bts/sio/paris2024/api/ApiService.kt
+++ app/src/main/java/bts/sio/paris2024/api/ApiService.kt
@@ -1,9 +1,13 @@
 package bts.sio.paris2024.api
 
 import bts.sio.paris2024.model.Actualite
+import bts.sio.paris2024.model.Epreuve
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("/actualites")
     suspend fun getActualites(): List<Actualite>
+
+    @GET("/epreuves")
+    suspend fun getEpreuves(): List<Epreuve>
 }
