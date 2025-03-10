2e29c7c702e716d21f54973bb5cd92c8d89bb00e
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-10 15:17:24 +0100

    Implémentation de l'api dans ActualiteViewModel

--- app/src/main/java/bts/sio/paris2024/model/viewsmodel/ActualiteViewModel.kt
+++ app/src/main/java/bts/sio/paris2024/model/viewsmodel/ActualiteViewModel.kt
@@ -1,8 +1,11 @@
 package bts.sio.paris2024.model.viewsmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import bts.sio.paris2024.model.Actualite
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewModelScope
+import bts.sio.paris2024.api.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,19 +16,32 @@ class ActualiteViewModel : ViewModel() {
     private val _actualites = MutableStateFlow<List<Actualite>>(emptyList())
     val actualites: StateFlow<List<Actualite>> = _actualites
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
     init {
-        // Simuler un chargement de données initiales
         getActualites()
     }
 
-    // Fonction pour simuler le chargement de bâtiments
     private fun getActualites() {
         viewModelScope.launch {
-            _actualites.value = listOf(
-                Actualite(1, "Records battus aux JO 2020", "Les records ont été battus dans diverses épreuves comme le 100m."),
-                Actualite(2, "Préparations pour les JO 2024", "Les équipes se préparent pour les jeux Olympiques à Paris."),
-                Actualite(3, "Nouvelle technologie en natation", "Des nouvelles technologies ont été utilisées pour améliorer la performance en natation.")
-            )
+            _isLoading.value = true
+            _errorMessage.value = null  // Réinitialise l'erreur avant l'appel
+
+            try {
+                val response = RetrofitInstance.api.getActualites()
+                _actualites.value = response
+            } catch (e: Exception) {
+                _errorMessage.value = "Erreur : ${e.localizedMessage ?: "Une erreur s'est produite"}"
+            } finally {
+                _isLoading.value = false
+                println("Chargement terminé")
+            }
         }
     }
+
+
 }
