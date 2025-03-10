8d008e848e1c84889fb80bc3ce0687c4ef4e1201
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-10 15:33:28 +0100

    Implémentation de l'api pour Epreuve

--- app/src/main/java/bts/sio/paris2024/model/views/epreuves/EpreuveList.kt
+++ app/src/main/java/bts/sio/paris2024/model/views/epreuves/EpreuveList.kt
@@ -3,10 +3,13 @@ package bts.sio.paris2024.model.views.epreuves
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import bts.sio.paris2024.model.viewsmodel.EpreuveViewModel
@@ -15,12 +18,16 @@ import bts.sio.paris2024.model.viewsmodel.EpreuveViewModel
 fun EpreuveList(viewModel: EpreuveViewModel = viewModel()) {
     // Observer les données de manière réactive
     val epreuves by viewModel.epreuves.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val errorMessage by viewModel.errorMessage.collectAsState()
 
-    LazyColumn(
-        modifier = Modifier.padding(8.dp)
-    ) {
-        items(epreuves) { epreuve ->
-            EpreuveCard(epreuve = epreuve)
+    when {
+        isLoading -> CircularProgressIndicator()
+        errorMessage != null -> Text(text = errorMessage!!, color = Color.Red)
+        else -> LazyColumn {
+            items(epreuves) { epreuve ->
+                EpreuveCard(epreuve = epreuve)
+            }
         }
     }
 }
