110c3d5379da51fad65fe537d19dea648a0b9112
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-17 13:35:48 +0100

    Mise à jour de la mise en page pour afficher le titre et la date sur la même ligne

--- app/src/main/java/bts/sio/paris2024/model/views/actualites/ActualiteCard.kt
+++ app/src/main/java/bts/sio/paris2024/model/views/actualites/ActualiteCard.kt
@@ -1,6 +1,7 @@
 package bts.sio.paris2024.model.views.actualites
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,13 +28,31 @@ fun ActualiteCard(actualite: Actualite) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
+            // Ligne avec le titre à gauche et la date à droite
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = actualite.titre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.weight(1f) // La colonne du titre prend tout l'espace restant
+                )
+                // Formatage de la date
+                val dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+                val formattedDate = actualite.date.format(dateFormatter)
+                Text(
+                    text = formattedDate,
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(start = 8.dp) // Un peu d'espace entre le titre et la date
+                )
+            }
 
-            val dateFormatter = DateTimeFormatter.ofPattern( "dd MMM yyyy")
-            val formattedDate = actualite.date.format(dateFormatter)
-
-            Text(text = actualite.titre, style = MaterialTheme.typography.bodyLarge)
-            Text(text = actualite.contenu, style = MaterialTheme.typography.bodyMedium)
-            Text(text = formattedDate, style = MaterialTheme.typography.bodySmall)
+            // Ligne avec le contenu
+            Text(
+                text = actualite.contenu,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 8.dp)
+            )
         }
     }
 }
