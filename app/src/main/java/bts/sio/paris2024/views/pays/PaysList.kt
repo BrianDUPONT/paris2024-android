f6eed473cd18161cea9132e41d0b14cb040c7555
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-25 15:56:33 +0100

    Amélioration de l'affichage de la liste des pays avec un bouton d'ajout plus visible et un affichage réactif des états de chargement et d'erreur.

--- app/src/main/java/bts/sio/paris2024/views/pays/PaysList.kt
+++ app/src/main/java/bts/sio/paris2024/views/pays/PaysList.kt
@@ -1,12 +1,21 @@
 package bts.sio.paris2024.views.pays
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -14,48 +23,58 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import bts.sio.paris2024.viewsmodel.PaysViewModel
 
 @Composable
-fun PaysList(
-    viewModel: PaysViewModel = viewModel(),
-    navController: NavController,
-    onAddPaysClick: () -> Unit
-) {
+fun PaysList(viewModel: PaysViewModel = viewModel(), navController: NavController) {
     // Observer les données de manière réactive
     val pays by viewModel.pays.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
     val errorMessage by viewModel.errorMessage.collectAsState()
 
     when {
-        isLoading -> {
-            CircularProgressIndicator()
-        }
-        errorMessage != null -> {
-            Text(text = errorMessage!!, color = Color.Red)
-        }
-        else -> {
-            Column {
-                // Ajouter un bouton pour ajouter un pays
-                Button(
-                    onClick = onAddPaysClick,
+        isLoading -> CircularProgressIndicator()
+        errorMessage != null -> Text(text = errorMessage!!, color = Color.Red)
+        else -> LazyColumn {
+            item {
+                Row(
                     modifier = Modifier
-                        .widthIn(min = 150.dp, max = 300.dp)
-                        .align(Alignment.CenterHorizontally)
-                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text("Ajouter un pays")
-                }
+                    Text(
+                        text = "Liste des pays",
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.primary
+                    )
 
-                // Afficher la liste des pays
-                LazyColumn {
-                    items(pays) { pays ->
-                        PaysCard(pays = pays, navController = navController)
+                    IconButton(
+                        onClick = {
+                            // Action lorsque le bouton + est cliqué
+                            // Par exemple, naviguer vers un écran pour ajouter un pays
+                            navController.navigate("add_pays")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Ajouter un pays",
+                            modifier = Modifier.size(64.dp)
+                        )
                     }
                 }
+
+            }
+
+            items(pays) { pays ->
+                PaysCard(pays = pays, navController = navController)
             }
         }
     }
